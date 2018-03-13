e9bc8a3f151fc2529fba214a3b2d0ddefa143769
Mike Hunsicker <hunsickm@matc.edu>
2018-03-12 19:10:35 -0500

    assignment-6 follow-along

---
+++ assignment-6/js/scripts.js
@@ -0,0 +1,61 @@
+var messages = [];
+
+function Message(type, user, text) {
+  this.type = type;
+  this.user = user;
+  this.text = text;
+}
+
+function addMessage(event) {
+  var user, type, className;
+  var messageInput = document.getElementById('message-input');
+  var messagesContainerEl = document.getElementById('message-container');
+
+  // Determine message type based on button clicked.
+  switch (event.target.id) {
+    case 'send-button':
+      user = 'Mike';
+      type = 'out';
+      className = 'out-message';
+      break;
+    case 'reply-button':
+      user = 'Joe';
+      type = 'in';
+      className = 'in-message';
+      break;
+    default:
+      user = 'unknown';
+      type = 'error';
+      className = 'error-message';
+  }
+
+  // Create new message.
+  if (messageInput.value != '') {
+    // Construct message and add to collection.
+    var message = new Message(type, user, messageInput.value);
+    messages.push(message);
+
+    // Create elements.
+    var messageText = document.createTextNode(message.text);
+    var messageSpanEl = document.createElement('span');
+    var messageDivEl = document.createElement('div');
+
+    messageSpanEl.appendChild(messageText);
+    messageDivEl.appendChild(messageSpanEl);
+    messageDivEl.className = className;
+
+    // Add to DOM.
+    messagesContainerEl.appendChild(messageDivEl);
+
+    // Reset input.
+    messageInput.value = '';
+  }
+}
+
+var init = function() {
+  // Wire event handlers
+  document.getElementById('send-button').onclick = addMessage;
+  document.getElementById('reply-button').onclick = addMessage;
+}
+
+init();
